f0824ab3406257b8db1c46c66bc04adf54b3d15e
John Saint-Fort <johnsaint-fort@Johns-MacBook-Pro.local>
2026-03-10 21:59:23 -0400

    Initial ASP.NET Core MVC project setup

---
+++ CampusServicesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CampusServicesApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
